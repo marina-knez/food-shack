a11fdef7d2b58c0e494555c58bb7bcc08bd3b1b1
Marina Knez <marina.knez08@gmail.com>
2023-11-24 14:35:21 +0100

    remove unused code

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,22 +1,10 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
 import { 
-    auth, 
     signInWithGooglePopup, 
     signInWithGoogleRedirect, 
     createUserDocumentFromAuth 
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-    useEffect(() => {
-        async function getUserFromRedirectResponse() {
-            const response = await getRedirectResult(auth);
-            if(response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user);
-            }
-        }
-        getUserFromRedirectResponse();
-    }, []);
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -28,9 +16,6 @@ const SignIn = () => {
             <h1>Sing In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in With Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in With Google Redirect
             </button>
         </div>
     )

65c661d6cfd400eaee3ac8f67055e807f277da5d
unknown <marina.knez08@gmail.com>
2024-05-28 13:58:14 +0200

    add home component

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,39 +1,10 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 
 const Home = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const appId = '9f54d0b7';
-    const appKey = '95d89b48a030319cb7919c0d77466d25';
-    
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${appId}&app_key=${appKey}`)
-      .then((res) => res.json())
-      .then((apiData) => setData(apiData))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
 
   return (
     <div>
-      {data &&
-        data.hits.map((item) => (
-          <div key={item.id}>
-            <h2>{item.recipe.label}</h2>
-            <img src={item.recipe.image} alt={item.recipe.label} />
-            <p>Ingredients:</p>
-            <ul>
-              {item.recipe.ingredientLines.map((ingredient, index) => (
-                <li key={index}>{ingredient}</li>
-              ))}
-            </ul>
-            <p>
-              <a href={item.recipe.url} target="_blank" rel="external noopener noreferrer">
-                Instructions
-              </a>
-            </p>
-            <p>Calories: {item.recipe.calories.toFixed(2)}</p>
-          </div>
-        ))}
+      <h1>Home page</h1>
     </div>
   );
 };

9e9989b853fe74176df4b91468b48ee569f030ac
unknown <marina.knez08@gmail.com>
2024-05-28 13:57:34 +0200

    add browserRouter and necessary providers

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { CategoriesProvider } from './contexts/categories.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <CategoriesProvider>
+          <App />
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>

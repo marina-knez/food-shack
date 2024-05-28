29a205f6719c828f7e08019cc6401527970c3af3
unknown <marina.knez08@gmail.com>
2024-05-28 13:56:53 +0200

    add basic routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
+import Recipes from './routes/recipes/recipes.component';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='/recipes/*' element={<Recipes />} />
       </Route>
     </Routes>
   );

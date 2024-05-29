2d468b12120e5307f1cb19df665bb10ad8428b0b
unknown <marina.knez08@gmail.com>
2024-05-29 13:53:03 +0200

    add new routes for adding, deleting and updating category

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Recipes from './routes/recipes/recipes.component';
+import AddCategory from './components/add-category/add-category.component';
+import UpdateCategory from './components/update-category/update-category.component';
+import DeleteCategory from './components/delete-category/delete-category.component';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route index element={<Home />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='/recipes/*' element={<Recipes />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/update-category/:oldCategoryName" element={<UpdateCategory />} />
+        <Route path="/delete-category/:categoryName" element={<DeleteCategory />} />
       </Route>
     </Routes>
   );

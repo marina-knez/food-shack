a364a601f7893e980bef0d2606cf773ceffabed7
unknown <marina.knez08@gmail.com>
2024-06-05 11:26:23 +0200

    adjust Category component to match new styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,6 +2,7 @@ import { useContext, Fragment } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import RecipeCard from '../../components/recipe-card/recipe-card.component';
+import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
@@ -10,18 +11,18 @@ const Category = () => {
 
     return (
         <Fragment>
-            <h2>{category.toUpperCase()}</h2>
+            <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
             <Link to={`/recipes/${category}/add-recipe`}>Add Recipe</Link>
-            {
-                recipes.map(recipe => (
-                    <div key={recipe.id}>
-                        <RecipeCard recipe={recipe} category={category} />
-                        <Link to={`/recipes/${category}/update-recipe/${recipe.id}`}>Update</Link>
-                        <br />
-                        <Link to={`/recipes/${category}/delete-recipe/${recipe.id}`}>Delete</Link>
-                    </div>
-                ))
-            }
+                {
+                    recipes.map(recipe => (
+                        <CategoryContainer key={recipe.id}>
+                            <RecipeCard recipe={recipe} category={category} />
+                            <Link to={`/recipes/${category}/update-recipe/${recipe.id}`}>Update</Link>
+                            <br />
+                            <Link to={`/recipes/${category}/delete-recipe/${recipe.id}`}>Delete</Link>
+                        </CategoryContainer>
+                    ))
+                }
         </Fragment>
     );
 };
---
+++ src/routes/category/category.styles.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const CategoryContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 20px;
+    row-gap: 50px;
+    margin: 50px;
+
+    @media screen and (max-width: 1024px) {
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+    }
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: repeat(1, 100%);
+        grid-template-rows: repeat(4, 1fr);
+    }
+`
+
+export const CategoryTitle = styled.h2`
+    font-size: 3rem;
+    text-align: center;
+`

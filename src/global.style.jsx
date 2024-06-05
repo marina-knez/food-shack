bc3b6c8a05bc07446a06d8bb57c9dd77172b7255
unknown <marina.knez08@gmail.com>
2024-06-05 09:00:33 +0200

    add global styles

---
+++ src/global.style.jsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Kalam', sans-serif;
+        overflow-x: hidden;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    * {
+        box-sizing: border-box;
+        scroll-behavior: smooth;
+    }
+
+    .arrow-up {
+        font-size: 1.5rem;
+        padding: 5px 10px;
+    }
+`

550bf3d5f93322d7dfa7b0ca075bacb97389c595
unknown <marina.knez08@gmail.com>
2024-06-05 11:27:20 +0200

    add Spinner component and include it in App.js as a preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { GlobalStyle } from './global.style';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
@@ -6,19 +8,23 @@ import Recipes from './routes/recipes/recipes.component';
 import AddCategory from './components/add-category/add-category.component';
 import UpdateCategory from './components/update-category/update-category.component';
 import DeleteCategory from './components/delete-category/delete-category.component';
+import Spinner from './components/spinner/spinner.component';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='/recipes/*' element={<Recipes />} />
-        <Route path="/recipes/add-category" element={<AddCategory />} />
-        <Route path="/recipes/update-category/:oldCategoryName" element={<UpdateCategory />} />
-        <Route path="/recipes/delete-category/:categoryName" element={<DeleteCategory />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+    <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='/recipes/*' element={<Recipes />} />
+          <Route path="/recipes/add-category" element={<AddCategory />} />
+          <Route path="/recipes/update-category/:oldCategoryName" element={<UpdateCategory />} />
+          <Route path="/recipes/delete-category/:categoryName" element={<DeleteCategory />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import { SpinnerOverlay, SpinnerContainer } from "./spinner.styles";
+
+const Spinner = () => {
+    return (
+        <SpinnerOverlay data-testid="spinner">
+            <SpinnerContainer />
+        </SpinnerOverlay>
+    )
+}
+
+export default Spinner;

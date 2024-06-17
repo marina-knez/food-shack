39ded2239d449ebb13479dea5aa0b9f3c90b2a77
unknown <marina.knez08@gmail.com>
2024-06-17 08:43:14 +0200

    add Random-recipe button

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useState, useContext } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
+import RecipeCard from '../../components/recipe-card/recipe-card.component';
 
 const Home = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [randomRecipe, setRandomRecipe] = useState(null);
+
+  const getRandomRecipe = () => {
+    const allRecipes = Object.values(categoriesMap).flat();
+    const randomIndex = Math.floor(Math.random() * allRecipes.length);
+    return allRecipes[randomIndex];
+};
+
+const handleRandomRecipe = () => {
+    const randomRecipe = getRandomRecipe();
+    setRandomRecipe(randomRecipe);
+};
 
   return (
     <div>
       <h1>Home page</h1>
+      <div>
+                <button onClick={handleRandomRecipe}>Get Random Recipe</button>
+            </div>
+
+            {randomRecipe && <RecipeCard recipe={randomRecipe} />}
     </div>
   );
 };

388e3b0f32f03d098cc84b686d4eb9cdbcfb0d10
unknown <marina.knez08@gmail.com>
2024-06-17 08:41:20 +0200

    add Load-more-btn; add RWD styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,29 +1,49 @@
-import { useContext, Fragment } from 'react';
+import { useContext, Fragment, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import RecipeCard from '../../components/recipe-card/recipe-card.component';
-import { CategoryContainer, CategoryTitle, AddRecipeLinkContainer, AddRecipeLink } from './category.styles';
+import Button, { BUTTON_TYPE_CLASSES } from '../../components/button/button.component';
+import { CategoryContainer, CategoryWrapper, RecipeCardContainer, CategoryTitle, AddRecipeLinkContainer, AddRecipeLink, LoadMoreButtonContainer } from './category.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Category = () => {
     const { category } = useParams();
     const { categoriesMap } = useContext(CategoriesContext);
     const recipes = categoriesMap[category.toLowerCase()] || [];
+    const [displayedRecipes, setDisplayedRecipes] = useState(4);
+
+    const handleLoadMore = () => {
+        setDisplayedRecipes(prevCount => prevCount + 4);
+    };
 
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
-                {
-                    recipes.map(recipe => (
-                        <div key={recipe.id}>
-                            <RecipeCard recipe={recipe} category={category} />
-                        </div>
-                    ))
-                }
+            <CategoryWrapper>
+                <CategoryContainer>
+                    {
+                        recipes
+                            .slice(0, displayedRecipes)
+                            .map(recipe => (
+                                <RecipeCardContainer key={recipe.id}>
+                                    <RecipeCard recipe={recipe} category={category} />
+                                </RecipeCardContainer>
+                        ))
+                    }
+                </CategoryContainer>
+                {displayedRecipes < recipes.length && (
+                <LoadMoreButtonContainer>
+                    <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={handleLoadMore}>Load More</Button>
+                </LoadMoreButtonContainer>
+                )}
                 <AddRecipeLinkContainer>
-                    <AddRecipeLink to={`/recipes/${category}/add-recipe`}>Add Recipe</AddRecipeLink>
+                    <AddRecipeLink to={`/recipes/${category}/add-recipe`}>
+                        <FontAwesomeIcon icon={faPlus} className='add' />
+                        Add Recipe
+                    </AddRecipeLink>
                 </AddRecipeLinkContainer>
-            </CategoryContainer>
+            </CategoryWrapper>
         </Fragment>
     );
 };
--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -1,24 +1,100 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { sharedButtonContainerStyles } from '../../components/add-category/add-category.styles';
 
-export const CategoryContainer = styled.div`
+export const sharedContainerStyles = css`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     column-gap: 30px;
     row-gap: 50px;
     align-items: center;
     justify-items: center;
-    margin: 20px 50px;
+    margin: 20px auto;
+    width: 100%;
 
     @media screen and (max-width: 1024px) {
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2, 1fr);
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+        margin: 20px auto;
     }
 
     @media screen and (max-width: 768px) {
-        grid-template-columns: repeat(1, 100%);
-        grid-template-rows: repeat(4, 1fr);
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr;
     }
+`
+
+export const sharedCardContainerStyles = css`
+    width: 100%;
+
+    @media screen and (max-width: 1024px) {
+        width: 90%;
+    }
+`
+
+export const sharedRecipeLinkContainerStyles = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 200px;
+    margin-left: 50px;
+    height: 70px;
+    width: 300px;
+    border: 4px solid rgba(240, 201, 240, 1);
+    border-radius: 1rem;
+    scale: 1;
+    transition: scale .5s;
+
+    &:hover {
+        scale: 1.1;
+    }
+
+    @media screen and (max-width: 1024px) {
+        margin-top: 30px;
+        margin-left: 25px;
+        width: 200px;
+        border-radius: .8rem;
+    }
+
+    @media screen and (max-width: 768px) {
+        margin: 30px auto;
+    }
+`
+
+export const sharedRecipeLinkStyles = css`
+    font-weight: 600;
+    font-size: 1.3rem;
+    color: rgba(168,96,168,1);
+    padding: 1rem;
+
+    & .add {
+        padding: 0 10px;
+    }
+
+    @media screen and (max-width: 768px) {
+        padding: .5rem;
+        font-size: 1rem;
+    }
+`
+
+export const CategoryWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 20px 50px;
+
+    @media screen and (max-width: 1024px) {
+        margin: 20px auto;
+    }
+`
+
+export const CategoryContainer = styled.div`
+${sharedContainerStyles}
+
+@media screen and (max-width: 768px) {
+    width: 90%;
+    margin: 20px auto;
+}
 `
 
 export const CategoryTitle = styled.h2`
@@ -28,18 +104,25 @@ export const CategoryTitle = styled.h2`
     color: rgba(133,44,133,1);
 `
 
+export const RecipeCardContainer = styled.div`
+    ${sharedCardContainerStyles}
+
+    @media screen and (max-width: 768px) {
+        width: 90%;
+    }
+`
+
 export const AddRecipeLinkContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 150px;
-    height: 200px;
-    border: 4px solid rgba(240, 201, 240, 1);
-    border-radius: 1rem;
+    ${sharedRecipeLinkContainerStyles}
+
+    margin-top: 50px;
+    margin-bottom: 50px;
 `
 
 export const AddRecipeLink = styled(Link)`
-    font-weight: 600;
-    color: rgba(168,96,168,1);
-    padding: 1rem;
+    ${sharedRecipeLinkStyles}
+`
+
+export const LoadMoreButtonContainer = styled.div`
+    ${sharedButtonContainerStyles}
 `

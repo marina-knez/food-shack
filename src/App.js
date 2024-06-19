fd8992faf3a33fdfecaf683bb149026469eda917
unknown <marina.knez08@gmail.com>
2024-06-19 12:35:47 +0200

    add route for shopping list

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './global.style';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+import PrivateRoute from './components/private-route/private-route.component';
 import Authentication from './routes/authentication/authentication.component';
 import Recipes from './routes/recipes/recipes.component';
 import AddCategory from './components/add-category/add-category.component';
 import UpdateCategory from './components/update-category/update-category.component';
 import DeleteCategory from './components/delete-category/delete-category.component';
+import ShoppingList from './routes/shopping-list/shopping-list.component';
 import Spinner from './components/spinner/spinner.component';
 import Button, { BUTTON_TYPE_CLASSES } from './components/button/button.component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -43,10 +45,27 @@ function App() {
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path='auth' element={<Authentication />} />
-          <Route path='/recipes/*' element={<Recipes />} />
-          <Route path="/recipes/add-category" element={<AddCategory />} />
-          <Route path="/recipes/update-category/:oldCategoryName" element={<UpdateCategory />} />
-          <Route path="/recipes/delete-category/:categoryName" element={<DeleteCategory />} />
+          <Route path='/recipes/*' element={
+            <PrivateRoute>
+              <Recipes />
+            </PrivateRoute>
+          } />
+          <Route path="/recipes/add-category" element={
+            <PrivateRoute>
+              <AddCategory />
+            </PrivateRoute>
+          } />
+          <Route path="/recipes/update-category/:oldCategoryName" element={
+            <PrivateRoute>
+              <UpdateCategory />
+            </PrivateRoute>
+          } />
+          <Route path="/recipes/delete-category/:categoryName" element={
+            <PrivateRoute>
+              <DeleteCategory />
+            </PrivateRoute>
+          } />
+          <Route path='/shopping-list' element={<ShoppingList />} />
         </Route>
       </Routes>
       {showScrollToTop && 

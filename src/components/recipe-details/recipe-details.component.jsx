932b395a1134737c33a7a6728259bf1ab82a9966
unknown <marina.knez08@gmail.com>
2024-06-19 12:32:54 +0200

    add functions for adding item to shopping list; adjust styles

--- src/components/recipe-details/recipe-details.component.jsx
+++ src/components/recipe-details/recipe-details.component.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
+import { ShoppingListContext } from '../../contexts/shoppingList.context';
 
 import { RecipeItemContainer, RecipeItemBasicsContainer, RecipeItemDetailsContainer, RecipeItemBasics, RecipeImgWrapper, RecipeItemTitle, RecipeItemInfoContainer, RecipeItemDescription, RecipeItemInfo, RecipeItemIngredientsContainer, RecipeItemInstructionsContainer, RecipeItemIngredientsTitle, RecipeIngredientsList, RecipeIngredientsListItem, RecipeInstructions, RecipeItemInstructionsTitle, RecipeInstructionsList, RecipeInstructionsStep, RecipeInstructionsListItem, NotFoundPageContainer, NotFoundText } from './recipe-details.styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,6 +11,7 @@ import { faSquare, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
 const RecipeDetails = () => {
     const { category, id } = useParams();
     const { categoriesMap } = useContext(CategoriesContext);
+    const { shoppingList, addToShoppingList, removeFromShoppingList } = useContext(ShoppingListContext);
     const recipe = categoriesMap[category]?.find((recipe) => recipe.id === parseInt(id));
     const [checkedItems, setCheckedItems] = useState(Array(recipe?.instructions.length).fill(false));
     const [itemsInCart, setItemsInCart] = useState(Array(recipe?.ingredients.length).fill(false));
@@ -26,17 +28,30 @@ const RecipeDetails = () => {
         setItemsInCart(prevItemsInCart => {
             const newItemsInCart = [...prevItemsInCart];
             newItemsInCart[index] = !newItemsInCart[index];
+            if (newItemsInCart[index]) {
+                addToShoppingList(recipe.title, recipe.ingredients[index]);
+            } else {
+                removeFromShoppingList(recipe.title, recipe.ingredients[index]);
+            }
             return newItemsInCart;
         });
     };
+
 
     if (!recipe) {
         return (
             <NotFoundPageContainer>
                 <NotFoundText>Recipe not found</NotFoundText>
-                <div style={{width: "100%", height: "0", paddingBottom: "61%", position: "relative"}}><iframe src="https://giphy.com/embed/SuHUqaOZM5GNz4hqCL" title='Recipe not found' style={{position:"absolute", height: "100%", width: "100%", frameBorder: "0"}} class="giphy-embed" allowFullScreen></iframe>
+                <div style={{ width: '100%', height: '0', paddingBottom: '61%', position: 'relative' }}>
+                    <iframe
+                        src="https://giphy.com/embed/SuHUqaOZM5GNz4hqCL"
+                        title="Recipe not found"
+                        style={{ position: 'absolute', height: '100%', width: '100%', frameBorder: '0' }}
+                        className="giphy-embed"
+                        allowFullScreen
+                    ></iframe>
                 </div>
-        </NotFoundPageContainer>
+            </NotFoundPageContainer>
     );
     }
 

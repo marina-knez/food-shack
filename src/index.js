d2702a1aaae150c7ab557bd2f3aab0d13b353757
unknown <marina.knez08@gmail.com>
2024-07-01 13:28:18 +0200

    import store to the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import './index.css';
 import App from './App';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { RecipesProvider } from './contexts/recipes.context';
 import { ShoppingListProvider } from './contexts/shoppingList.context';
@@ -12,16 +13,16 @@ import { ShoppingListProvider } from './contexts/shoppingList.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <RecipesProvider>
-            <ShoppingListProvider>
-              <App />
-            </ShoppingListProvider>
-          </RecipesProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+          <CategoriesProvider>
+            <RecipesProvider>
+              <ShoppingListProvider>
+                <App />
+              </ShoppingListProvider>
+            </RecipesProvider>
+          </CategoriesProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

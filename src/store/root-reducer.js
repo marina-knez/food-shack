8aa77326fbb4fecaf56a31e07a92c0b9591dffd1
unknown <marina.knez08@gmail.com>
2024-07-03 15:10:06 +0200

    add categoriesReducer and recipesReducer to root-reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
+import { categoriesReducer } from "./categories/category.reducer";
+import { recipesReducer } from "./recipes/recipe.reducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
+    categories: categoriesReducer,
+    recipes: recipesReducer
 })

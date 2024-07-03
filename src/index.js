13b9c4a1950f83f42d37d88bf9a3cbb87f9416ef
unknown <marina.knez08@gmail.com>
2024-07-03 15:09:31 +0200

    remove used contexts for categories and recipes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import { store } from './store/store';
 
 import './index.css';
 import App from './App';
-import { CategoriesProvider } from './contexts/categories.context';
-import { RecipesProvider } from './contexts/recipes.context';
 import { ShoppingListProvider } from './contexts/shoppingList.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,13 +13,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
-            <RecipesProvider>
-              <ShoppingListProvider>
-                <App />
-              </ShoppingListProvider>
-            </RecipesProvider>
-          </CategoriesProvider>
+        <ShoppingListProvider>
+          <App />
+        </ShoppingListProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>

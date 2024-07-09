3f394429a2a6384b4e4fbbedc7e1bdb4c59da6e6
unknown <marina.knez08@gmail.com>
2024-07-09 12:23:47 +0200

    remove unused code, make adjustments for middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import thunk from "redux-thunk";
 import { rootReducer } from "./root-reducer";
@@ -10,21 +12,22 @@ const actionSanityCheck = store => next => action => {
     return next(action);
 };
 
-const middleWares = [actionSanityCheck, logger, thunk];
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['shoppingList']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, actionSanityCheck, thunk].filter(Boolean);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(
-    rootReducer, 
+    persistedReducer, 
     undefined, 
     composedEnhancers
 );
 
-//const middleWares = [logger, thunk];
-
-//const composedEnhancers = compose(applyMiddleware(...middleWares));
-
-/*export const store = createStore(
-    rootReducer, 
-    undefined, 
-    composedEnhancers
-);*/
+export const persistor = persistStore(store);

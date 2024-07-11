9fb85f826e1d772fb4e514b334ad9141b49ef4a5
unknown <marina.knez08@gmail.com>
2024-07-11 10:06:03 +0200

    fix issues with middlewares

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,8 @@
-import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import { compose, createStore, applyMiddleware, Middleware, Dispatch, AnyAction } from "redux";
 import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from "redux-thunk";
+import thunk, { ThunkMiddleware } from "redux-thunk";
 import { rootReducer } from "./root-reducer";
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -19,7 +19,13 @@ const persistConfig: ExtendedPersistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter((middleware): middleware is Middleware => Boolean(middleware));
+type CustomMiddleware = Middleware<{}, RootState, Dispatch<AnyAction>>;
+
+const middleWares: CustomMiddleware[] = [thunk as ThunkMiddleware<RootState, AnyAction>];
+
+if (process.env.NODE_ENV !== 'production') {
+    middleWares.push(logger as CustomMiddleware);
+}
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
